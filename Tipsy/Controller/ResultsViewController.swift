7cf565cf28f76c28c5767706722ceb2e9659db2e
alieren97 <alierensh3@gmail.com>
2021-11-23 02:02:37 +0300

    first commit

---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Ali Eren on 20.11.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
